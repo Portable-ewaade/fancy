c2aed929507b510a4ddc5b48f5cfa62fbdd6d31d
a&co consulting <sysadmin@theacconsulting.com>
2024-01-19 12:06:18 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 //   reactStrictMode: true,
 // }
 const nextConfig = {
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'dist',
