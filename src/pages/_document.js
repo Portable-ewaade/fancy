0170be581302216b5494a2f2647f759a077ab7a3
Kehinde Adetule <kehinde.adetule@theacconsulting.com>
2024-02-07 22:20:27 +0100

    review

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-YE9SJBBG4L"></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-YE9SJBBG4L');
-        </script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
